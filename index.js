92904370b9aa2765a3132a4b654a7f114297fe54
Ileossa <you@example.com>
2016-12-01 03:16:06 +0100

    test launch docker

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// standby du server
 const server = app.listen(3000, () => {  
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);});
+
+//
+// config pour executer les scripts shell
+var sys = require('sys')
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+
 
 // 
 //   COMMANDES FROM USER 
@@ -22,19 +30,26 @@ app.post('/', (req, res) => {
 
 // traitement et reponse
 function handleQueries(q, res){
-	let data = 
-	{
-		response_type: 'in_channel',
-		text: '302: Found',
-		attachments:[
-			{
-				image_url: 'https://http.cat/302.jpg'
-			}
-		]
-	};
-	res.json(data);
-
-
+	let text = q.text;
 	
+	if( text == "toto"){
+		res.send("tu as dis toto");
+		return;
+	}
+	if( text == "launch docker"){
+		exec("sh scripts/launcher.sh");
+		res.send("Launch on <http://localhost:8080>");
+	}
+	else{
+		let data = {
+			response_type: 'ephemeral', // private message
+			text: 'How to use /httpstatus command:',
+			attachments:[
+			{
+				text: 'Type a status code after the command, e.g. `/httpstatus 404`',
+			}
+			]
+		};
+		res.json(data);
+	}
 }
-
